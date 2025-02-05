54ad23aa1738a81856bd8d881a2b87b2ab491bf9
aktranformer123 <tuanhocgioi101@gmail.com>
2025-02-05 22:42:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,16 +5,16 @@ const CONFIG = {
     Một lời yêu chẳng dám tỏ bày  
     Hôm nay mượn chút gió mây  
     Nhờ câu chữ gửi đến người đây! 💖`,
-    btnIntro: 'Bấm vào đây nè ^^',
+    btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em chính là định mệnh của đời anh? 🥰 ',
-    btnYes: 'Ngay từ khoảnh khắc đầu tiên, anh đã biết trái tim mình thuộc về em rồi! 💕',
-    btnNo: 'Em cũng thích anh lắm <33',
-    question: 'Không nha, anh chỉ là bạn :3',
-    btnReply: 'Giữa hàng tỷ người trên thế giới, tại sao em lại chọn anh? ❤️',
-    reply: 'Gửi cho anh <3',
-    mess: 'Vì anh là người đặc biệt nhất, là người em luôn nghĩ đến! <333',
-    messDesc: '14/2 này, anh mời em một buổi hẹn hò đặc biệt nhé! ❤️',
-    btnAccept: 'Dạ vâng, em đồng ý <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Hẹn em 14/2 mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/hoang.tuan.369414/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
