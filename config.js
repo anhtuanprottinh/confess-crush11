55afa88a62d26c53c3ea79904ec5417aae2f0a12
aktranformer123 <tuanhocgioi101@gmail.com>
2025-02-05 22:39:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Gửi cho người anh thương nhất",
+    introTitle: 'Gửi em, người đặc biệt',
+    introDesc: `Anh đã giữ trong tim bao lâu nay  
+    Một lời yêu chẳng dám tỏ bày  
+    Hôm nay mượn chút gió mây  
+    Nhờ câu chữ gửi đến người đây! 💖`,
+    btnIntro: 'Bấm vào đây nè ^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    desc: 'Phải chăng em chính là định mệnh của đời anh? 🥰 ',
+    btnYes: 'Ngay từ khoảnh khắc đầu tiên, anh đã biết trái tim mình thuộc về em rồi! 💕',
+    btnNo: 'Em cũng thích anh lắm <33',
+    question: 'Không nha, anh chỉ là bạn :3',
+    btnReply: 'Giữa hàng tỷ người trên thế giới, tại sao em lại chọn anh? ❤️',
+    reply: 'Gửi cho anh <3',
+    mess: 'Vì anh là người đặc biệt nhất, là người em luôn nghĩ đến! <333',
+    messDesc: '14/2 này, anh mời em một buổi hẹn hò đặc biệt nhé! ❤️',
+    btnAccept: 'Dạ vâng, em đồng ý <3',
+    messLink: 'https://www.facebook.com/hoang.tuan.369414/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
